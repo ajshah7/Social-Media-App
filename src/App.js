05cb3680b4c5fb15b89b09342cd124f32a9c79f7
Ajmal Shah <31572007+ajshah7@users.noreply.github.com>
2020-10-23 13:36:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,17 +10,21 @@ import "./App.css";
 import SignIn from "../src/componets/Authentication/SignIn";
 import SignUp from "../src/componets/Authentication/SignUp";
 import Feeds from "../src/componets/FeedsPage/Feeds";
+import ViewProfile from "./componets/container/ViewProfile";
+import ViewFeed from "./componets/container/ViewFeed";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <HashRouter>
-          <Switch>
-            <Route exact path="/" component={SignIn} />
-            <Route exact path="/sign-up" component={SignUp} />
-            <Route exact path="/feeds" component={Feeds} />
-          </Switch>
+        <Switch>
+          <Route exact path="/" component={SignIn} />
+          <Route exact path="/sign-up" component={SignUp} />
+          <Route exact path="/feeds" component={Feeds} />
+          <Route exact path="/user/:id" component={ViewProfile} />
+          <Route exact path="/feed/:id" component={ViewFeed} />
+        </Switch>
         </HashRouter>
       </Router>
     </div>
